ed16c08a5bd9e1d233bef7179a2de388435db185
HEBER-PC\heber <heber.navegante@gmail.com>
2018-11-26 22:38:42 -0200

    Adicionando e usando o VueRouter

--- Udemy/frontend/src/main.ts
+++ Udemy/frontend/src/main.ts
@@ -2,6 +2,8 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import NoticiasComponent, { NoticiasRoute } from "./components/noticias.component.vue";
 
+Vue.use(VueRouter);
+
 Vue.component('noticias', NoticiasComponent);
 
 let router = new VueRouter({
