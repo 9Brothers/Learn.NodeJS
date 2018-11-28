96c50673e4fd0ddbd3bdf4e5d975a4eb0098f257
HEBER-PC\heber <heber.navegante@gmail.com>
2018-11-27 23:24:58 -0200

    ajuda

--- Udemy/frontend/src/components/home.component.ts
+++ Udemy/frontend/src/components/home.component.ts
@@ -1,7 +1,8 @@
 // import Vue, { ComponentOptions } from "vue";
 import { Requester } from "../../../shared/utils/Requester";
+import { AsyncComponent } from "vue";
 
-export const HomeComponent = (resolve: any, rejects: any) => {
+export const HomeComponent: AsyncComponent = (resolve, rejects) => {
   Requester.GetHTML<string>('/templates/home.template.html')
   .then((value) => {
     let nome = 'Heber';
